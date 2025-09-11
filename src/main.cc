d3336e02a93ab7e547e20f94bd4348cf1797b7e5
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-12 00:52:41 +0300

    Implement basic plot

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <matplot/freestanding/plot.h>
+
+#include <vector>
+#include <numbers>
+#include <cmath>
+#include <ranges>
 
 int main(const int argc, const char* argv[]) {
-  std::cout << "Hello word!\n";
+  std::vector<double> x_numbers;
+  std::vector<double> y_numbers;
+  for (const double x: matplot::linspace(0, 2 * std::numbers::pi)) {
+    x_numbers.push_back(std::cos(x));
+    y_numbers.push_back(std::sin(x));
+  }
+
+  matplot::plot(x_numbers, y_numbers);
+  matplot::show();
   return 0;
 }
