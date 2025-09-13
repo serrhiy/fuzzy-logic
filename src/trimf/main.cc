8288e6af803d3b80440007ea0955cd57adbc81c7
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-13 03:18:44 +0300

    Add trimf

---
+++ src/trimf/main.cc
@@ -0,0 +1,32 @@
+#include <matplot/freestanding/plot.h>
+#include <matplot/axes_objects/line.h>
+#include <matplot/freestanding/axes_lim.h>
+#include <matplot/util/common.h>
+
+#include <memory>
+#include <vector>
+#include <utility>
+#include <ranges>
+#include <functional>
+
+double trimf(const double x, const double a, const double b, const double c) {
+  if (x < a) return 0.0;
+  if (x >= a && x < b) return (x - a) / (b - a);
+  if (x >= b && x <= c) return (c - x) / (c - b);
+  return 0.0;
+}
+
+std::function<double(const double)> makeTrimf(const double a, const double b, const double c) {
+  return [a, b, c](const double x) -> double { return trimf(x, a, b, c); };
+}
+
+int main(const int argc, const char* argv[]) {
+  std::vector<double> x_numbers = matplot::linspace(-5, 5);
+  std::vector<double> y_numbers =
+    x_numbers | std::views::transform(makeTrimf(-3, 0, 3)) | std::ranges::to<std::vector>();
+
+  matplot::plot(x_numbers, y_numbers)->line_width(2);
+  matplot::ylim({ 0, 2 });
+  matplot::show();
+  return 0;
+}
